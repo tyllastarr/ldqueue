5232ef1558c3713dde73a2d192238af8ce4b5d88
Tyll'a <psduckie@yahoo.com>
2020-04-23 14:18:29 -0400

    Wrote basic JS file.

---
+++ index.js
@@ -0,0 +1,83 @@
+const Discord = require("discord.js");
+const client = new Discord.Client;
+const config = require("./config.json");
+const mysqlx = require("@mysql/xdevapi");
+const log4js = require("log4js");
+var dbSession;
+
+log4js.configure({
+    appenders: {
+        file: { type: "file", filename: "ldqueue.log" },
+        out: { type: "stdout" }
+    },
+    categories: { default: { appenders: ["file", "out"], level: "debug" } }
+});
+const logger = log4js.getLogger();
+
+client.once("ready", () => {
+    mysqlx.getSession({
+        user: config.dbUser,
+        password: config.dbPassword,
+        host: config.dbHost,
+        port: config.dbPort,
+        schema: config.dbSchema
+    }).then(session => {
+        dbSession = session;
+    });
+    logger.info("Ready!");
+});
+
+client.login(config.token);
+
+function checkForEmpty() {
+    dbSession.sql("SELECT COUNT(*) FROM LDQueue").execute(row => {
+        if (row[0] > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+}
+
+function getMin() {
+    dbSession.sql("SELECT MIN(QueuePosition) FROM LDQueue").execute(row => {
+        return row[0];
+    });
+}
+
+client.on("message", message => {
+    if (message.content.startsWith(`${config.prefix}add`)) {
+        var link = message.content.slice(5);
+        dbSession.sql(`INSERT INTO LDQueue(QueueLink) VALUES(${link})`).execute();
+    }
+    if (message.content.startsWith(`${config.prefix}next`)) {
+        if (message.member.roles.exists("name", "LDer")) {
+            var empty = checkForEmpty();
+            if (empty == false) {
+                var min = getMin();
+                dbSession.sql(`SELECT QueueLink FROM LDQueue WHERE QueuePosition=${min}`).execute(row => {
+                    message.reply(`the next game is at ${row[0]}.`);
+                });
+            }
+            else {
+                message.reply("queue is empty.");
+            }
+        } else {
+            message.reply("access denied.");
+        }
+    }
+    if (message.content.startsWith(`${config.prefix}delete`)) {
+        if (message.member.roles.exists("name", "LDer")) {
+            var empty = checkForEmpty();
+            if(empty == false) {
+                var min = getMin();
+                dbSession.sql(`DELETE FROM LDQueue WHERE QueuePosition=${min}`).execute();
+            }
+            else {
+                message.reply("queue is empty.");
+            }
+        } else {
+            message.reply("access denied.");
+        }
+    }
+})
